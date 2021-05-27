85b2da0549236acde42c29b163f9ebe032ee513e
lihbr <lihbr@users.noreply.github.com>
2021-05-27 12:47:57 +0200

    feat: setup cli and helpers

---
+++ src/cli.ts
@@ -0,0 +1,59 @@
+import cac from "cac";
+import exit from "exit";
+import latestVersion from "latest-version";
+import semver from "semver";
+
+import * as commands from "./commands";
+import { logger, NAME, PACKAGE, ucFirst, VERSION } from "./lib";
+
+const cli = cac(NAME);
+
+cli.command("").action(options => {
+	commands._default(cli, options);
+});
+
+cli.command("login <token>", "Log in to Dico.app").action(async options => {
+	await commands.signin(cli, options);
+});
+
+cli.command("logout", "Log out of Dico.app").action(options => {
+	commands.signout(cli, options);
+});
+
+cli.command("whoami", "Display current user").action(async options => {
+	await commands.whoami(cli, options);
+});
+
+cli.version(VERSION);
+cli.help(commands.help);
+
+const run = async (): Promise<void> => {
+	try {
+		cli.parse(process.argv, { run: false });
+		await cli.runMatchedCommand();
+	} catch (error) {
+		if (error.message && typeof error.message === "string") {
+			logger.error(ucFirst(error.message));
+		} else {
+			logger.fatal("");
+			console.log(error);
+			console.log("");
+		}
+		exit(1);
+	}
+
+	const version = await latestVersion(PACKAGE);
+	if (semver.gt(version, VERSION)) {
+		console.log("");
+		logger.info(
+			`A new version of \`${PACKAGE}\` is available!\n\n  Install it with:\n  $ npm install --global ${PACKAGE}@${version}\n`
+		);
+	}
+};
+
+process.on("unhandledRejection", error => {
+	logger.fatal(error);
+	exit(2);
+});
+
+run();
---
+++ src/lib/const.ts
@@ -0,0 +1,9 @@
+import pkg from "../../package.json";
+
+export const NAME = "dico";
+export const PACKAGE = pkg.name;
+export const DESCRIPTION = pkg.description;
+export const VERSION = pkg.version;
+export const RC_FILE = ".dicorc";
+export const API_ENDPOINT = "http://localhost:3000/v1/cli";
+// export const API_ENDPOINT = "https://api.dico.app/v1/cli";
