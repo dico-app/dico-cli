8e9d00a58bbc6fa38af50ccf2f6a5603a13979c3
lihbr <lihbr@users.noreply.github.com>
2021-06-03 09:07:47 +0200

    feat: provide line and column information when crawling files

--- src/core/project.ts
+++ src/core/project.ts
@@ -4,6 +4,7 @@ import path from "path";
 import * as dicojson from "./dicojson";
 import globby from "globby";
 import { SOURCES_EXTRACT_REGEX } from "../const";
+import { ProjectKey } from "../types";
 
 export const findSources = async (sources?: string[]): Promise<string[]> => {
 	if (!sources) {
@@ -15,12 +16,28 @@ export const findSources = async (sources?: string[]): Promise<string[]> => {
 	return paths;
 };
 
-export const crawlFile = (relativePath: string): string[] => {
+export const crawlFile = (relativePath: string): ProjectKey[] => {
 	const absolutePath = path.join(process.cwd(), relativePath);
 
 	const blob = fs.readFileSync(absolutePath, "utf8");
 
-	const matches = blob.match(SOURCES_EXTRACT_REGEX);
+	const matches: ProjectKey[] = [];
 
-	return matches ? matches : [];
+	const regex = new RegExp(SOURCES_EXTRACT_REGEX);
+	let line = 1;
+	let index = 0;
+	let match;
+	while ((match = regex.exec(blob))) {
+		const section = blob.slice(index, match.index).split("\n");
+		line += section.length - 1;
+		index = match.index;
+		matches.push({
+			key: match[0],
+			file: relativePath,
+			line,
+			column: (section.pop() || "").length + 1
+		});
+	}
+
+	return matches;
 };
